1108826d410c67d6aaf9bcf099f421d0a544b9b8
Fabio <clubeteczagem@gmail.com>
2022-02-21 07:56:03 -0300

    Typescript2

---
+++ dist/js/enums/dias-da-semana.js
@@ -0,0 +1,10 @@
+export var DIasDaSemana;
+(function (DIasDaSemana) {
+    DIasDaSemana[DIasDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DIasDaSemana[DIasDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DIasDaSemana[DIasDaSemana["TER\u00C7A"] = 2] = "TER\u00C7A";
+    DIasDaSemana[DIasDaSemana["QUARTA"] = 3] = "QUARTA";
+    DIasDaSemana[DIasDaSemana["QUINTA"] = 4] = "QUINTA";
+    DIasDaSemana[DIasDaSemana["SEXTA"] = 5] = "SEXTA";
+    DIasDaSemana[DIasDaSemana["SABADO"] = 6] = "SABADO";
+})(DIasDaSemana || (DIasDaSemana = {}));
